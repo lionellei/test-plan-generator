0a325b36879ce4fe026781a02895c38bbc6be618
lionellei <lionel_lei@yahoo.com>
2014-11-13 07:51:30 -0800

    testing submit when enter hit

--- client/templates/testgroup/new_test_row.js
+++ client/templates/testgroup/new_test_row.js
@@ -1,8 +1,22 @@
 if (Meteor.isClient) {
     Template.new_test_row.events({
-       "keyup .input-sm input": function (event, template) {
-           console.log("key up detected");
-           //console.log(event.name);
+       "keyup .test_item_input": function (event, template) {
+           //console.log(event.target.name);
+           if (event.keyCode == 13) {
+               /*
+               var test_item = {};
+               test_item[event.target.name]=event.target.value;
+               _id = Testitems.insert(test_item);
+               test_item = Testitems.findOne(_id);
+               console.log(test_item); */
+               //console.log(event);
+               console.log(template);
+               console.log(template.firstNode.children);
+               cells = template.firstNode.children;
+               for (var i = 0; i < cells.length; i++) {
+                   console.log(cells[i].firstChild.value);
+               }
+           }
        } 
     });
 }
