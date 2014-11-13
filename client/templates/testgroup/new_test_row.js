1e878f98fdc941821db9848c087c987808be8d5d
Lionel Li <lionel_lei@yahoo.com>
2014-11-13 20:57:17 +0000

    commiting fooling around

--- client/templates/testgroup/new_test_row.js
+++ client/templates/testgroup/new_test_row.js
@@ -1,8 +1,14 @@
 if (Meteor.isClient) {
     Template.new_test_row.events({
-       "keyup .input-sm input": function (event, template) {
+        /*
+       "keyup .input-sm-action input": function (event, template) {
+           alert("key up detected!");
            console.log("key up detected");
            //console.log(event.name);
-       } 
+       } */
+
+       "keyup .input-sm :input": function(){
+           alert("you typed.");
+       }
     });
 }
