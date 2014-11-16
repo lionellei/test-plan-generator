377dc7b1217c9386e4e91b6035b49cfa19c67968
lionellei <lionel_lei@yahoo.com>
2014-11-15 22:14:13 -0800

    modified gmail smtp setting

--- server/smtp.js
+++ server/smtp.js
@@ -1,9 +1,9 @@
 Meteor.startup(function () {
     smtp = {
-        username: 'tplab.finisar',   // eg: server@gentlenode.com
+        username: 'tplab.finisar@gmail.com',   // eg: server@gentlenode.com
         password: '1389moffett',   // eg: 3eeP1gtizk5eziohfervU
         server:   'smtp.gmail.com',  // eg: mail.gandi.net
-        port: 25
+        port: 465
     };
 
     process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
