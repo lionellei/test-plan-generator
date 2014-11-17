ddb21e6a4f8ecb0308608ab97a043e00d25bd967
lionellei <lionel_lei@yahoo.com>
2014-11-16 21:46:03 -0800

    used autoform for chip name input

--- client/templates/dashboard/dashboard.js
+++ client/templates/dashboard/dashboard.js
@@ -4,25 +4,11 @@
 
 
 //////////// Partials /////////////////
-var chipNameSubmit = function(chip_name){
-    console.log("chip name is "+chip_name);
-    $('.chip-name-modal').modal('hide');
-};
-
-Template.newTestPlanForm.events({
-    // User hit enter on the input field
-    // TODO: check if input field is empty.
-    "keyup .chip-name-input": function(event, template) {
-        if (event.keyCode==13) {
-            var chip_name = $('.chip-name-input')[0].value;
-            chipNameSubmit(chip_name);
+AutoForm.hooks({
+    chipNameForm: {
+        onSuccess: function(operation, result, template){
+            // Dismiss the modal.
+            $('.chip-name-modal').modal('hide');
         }
-    },
-
-    // User click submit button
-    // TODO: check if input field is empty.
-    "click .chip-name-input-submit": function(event, template) {
-        var chip_name = $('.chip-name-input')[0].value;
-        chipNameSubmit(chip_name);
     }
 });
