22c591470edcfba84a336848ba2114b9f18cd662
lionellei <lionel_lei@yahoo.com>
2014-11-16 22:06:11 -0800

    implemented using chipName as testplan path id, and route to testplan once it's created

--- client/templates/dashboard/dashboard.js
+++ client/templates/dashboard/dashboard.js
@@ -9,6 +9,10 @@ AutoForm.hooks({
         onSuccess: function(operation, result, template){
             // Dismiss the modal.
             $('.chip-name-modal').modal('hide');
+
+            //result is the _id of the generated document.
+            var testplan = {chipName: Testplans.findOne(result).chipName};
+            Router.go('testplanPage', testplan);
         }
     }
 });
