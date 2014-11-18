667f8031f30ae0c600ed0747a82afa3194b1dfcb
lionellei <lionel_lei@yahoo.com>
2014-11-17 23:41:09 -0800

    mocked up add test group modal

--- client/templates/testplan/testplanPage.js
+++ client/templates/testplan/testplanPage.js
@@ -1,1 +1,35 @@
+// ********* Main Template *************
 
+
+
+// ********* Partials ******************
+Template.newTestGroupForm.helpers({
+   "hiddenOrShow": function () {
+       // Use .prob('checked') instead of .checked for the jquery selector
+       /*
+       if ($('.select-custom-test').prop('checked')) {
+           return "show";
+       } else {
+           return "hidden";
+       }*/
+
+       // Or this way:
+       // return $('.select-custom-test').prop('checked')? "show" : "hidden";
+
+       // TODO: Why the two ways above is not reactive? Resort to this hacky way below:
+       return Session.get('showCustomTestNameInput')? "show" : "hidden";
+   },
+
+   "customTestChecked": function () {
+       return Session.get('showCustomTestNameInput');
+   }
+});
+
+Template.newTestGroupForm.events({
+   // Use the change event instead of click because this checkbox could change programatically
+   // i.e., checking any of the template checkbox will unchecked this checkbox.
+   "change .select-custom-test": function() {
+       // Toggle the 'showCustomTestNameInput' key:
+       Session.set('showCustomTestNameInput', !Session.get('showCustomTestNameInput'));
+   }
+});
