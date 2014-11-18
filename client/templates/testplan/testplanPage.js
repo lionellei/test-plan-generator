0fdf10c52857e0b5224f8a9063c0ef12fef8320d
Lionel Li <lionel_lei@yahoo.com>
2014-11-18 20:59:45 +0000

    implemented getTestGroupByName function

--- client/templates/testplan/testplanPage.js
+++ client/templates/testplan/testplanPage.js
@@ -56,6 +56,7 @@ Template.newTestGroupForm.events({
            if ($('#select-continuity-template').prop('checked')) {
                // Do something to create continuity tests
                console.log('Create continuity test');
+               generateContinuityTests(this);
            }
            
            if ($('#select-leakage-template').prop('checked')) {
@@ -67,3 +68,58 @@ Template.newTestGroupForm.events({
        $('.add-test-group-modal').modal('hide');
    }
 });
+
+var getTestGroupByName = function(testGroupName, chipName, testplanId) {
+    var testgroup = Testgroups.findOne({name:testGroupName, testplanId:testplanId});
+    if (!testgroup) { // No testgroup in this testplan that has the given name
+        // create it
+        testgroup = {
+          name: testGroupName,
+          chipName: chipName,
+          testplanId: testplanId
+        };
+        testgroup._id = Testgroups.insert(testgroup);
+    }
+    return testgroup;
+}
+
+var generateContinuityTests = function(chipObj) { 
+    // chipObj has the following data:
+    // Object { _id: "MQ9qAHguc24ip3gw5", chipName: "qTIAX1B" }
+    // TODO: Does it need to take a collection as argument?
+    
+    var pads = Pads.find({chipName:chipObj.chipName}).fetch();
+    if (pads.length > 0) { //check if there is Pads list first!!
+        console.log("Generating continuity test for "+chipObj.chipName);
+        
+        // First create or retrieve the testgroup with the given name
+        var continuityTest = getTestGroupByName("Continuity", chipObj.chipName, chipObj._id);
+        console.log(continuityTest);
+        // Loops through all the pads
+        /*
+        for (var i = 0; i < pads.length; i++) {
+            // Only create the test_item for the pad if there is none already.
+            // Pads list sometimes will have same pads appear more than once.
+            // TODO: In reality, should check within the same testgroup instead of the entire collection.
+            pad = pads[i];
+            if (!!!Testitems.findOne({pad:pad.name})) {
+                var test_item = {
+                    "pad": pad.name,
+                    "resource": prototype_test.resource,
+                    "source_type": prototype_test.source_type,
+                    "source_value": prototype_test.source_value,
+                    "source_unit": prototype_test.source_unit,
+                    "compliance_type": prototype_test.compliance_type,
+                    "compliance_value": prototype_test.compliance_value,
+                    "compliance_unit": prototype_test.compliance_unit,
+                    "measure_type": prototype_test.measure_type,
+                    "measure_min": prototype_test.measure_min,
+                    "measure_typ": prototype_test.measure_typ,
+                    "measure_max": prototype_test.measure_max,
+                    "measure_unit": prototype_test.measure_unit
+                };
+                Testitems.insert(test_item);
+            }
+        } */
+    }
+};
