057fe14de6b0b0ea648eae5a89af3daa624fc925
Lionel Li <lionel_lei@yahoo.com>
2014-11-18 00:35:02 +0000

    use Session variable to pass the chipName to spreadsheet-to-mongo package for the save call back

--- client/templates/pads/pads_list.js
+++ client/templates/pads/pads_list.js
@@ -1,14 +1,15 @@
 
 Template.padsList.helpers({
-   /*
-   "pads": function() {
-      //console(data);
-      return Pads.find();
-   }
-   */
    log: function() {
       console.log(this);
       console.log(this.fetch());
+   },
+   
+   // A hacky way to let spreadsheet-to-mongo package call back to know the chipName
+   setChipNameInSession: function() {
+      //console.log(this.matcher._selector.chipName);
+      Session.set('currentChipName', this.matcher._selector.chipName);
+      console.log(Session.get('currentChipName'));
    }
 });
 
