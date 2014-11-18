34642180ebadc5c509419e9ee107754528464710
Lionel Li <lionel_lei@yahoo.com>
2014-11-18 20:05:03 +0000

    move account email verification stuff to dashboard.js, because it should be on the template that '/' routes too

--- client/templates/dashboard/dashboard.js
+++ client/templates/dashboard/dashboard.js
@@ -1,6 +1,18 @@
 // Main Template ///////
-
-
+///////////// This is for Account verification, it should be in the template that '/' routes to.
+Template.dashboard.created = function() {
+    if (Accounts._verifyEmailToken) {
+        Accounts.verifyEmail(Accounts._verifyEmailToken, function(err) {
+            if (err != null) {
+                if (err.message = 'Verify email link expired [403]') {
+                    console.log('Sorry this verification link has expired.')
+                }
+            } else {
+                console.log('Thank you! Your email address has been confirmed.')
+            }
+        });
+    }
+};
 
 
 //////////// Partials /////////////////
@@ -16,3 +28,4 @@ AutoForm.hooks({
         }
     }
 });
+
--- client/templates/testgroup/testgroup.js
+++ client/templates/testgroup/testgroup.js
@@ -34,18 +34,3 @@ Template.testgroup.events({
       saveAs(blob, "testplan.csv");
    }
 });
-
-///////////// This is for Account verification, it should be in the template that '/' routes to.
-Template.testgroup.created = function() {
-    if (Accounts._verifyEmailToken) {
-        Accounts.verifyEmail(Accounts._verifyEmailToken, function(err) {
-            if (err != null) {
-                if (err.message = 'Verify email link expired [403]') {
-                    console.log('Sorry this verification link has expired.')
-                }
-            } else {
-                console.log('Thank you! Your email address has been confirmed.')
-            }
-        });
-    }
-};
