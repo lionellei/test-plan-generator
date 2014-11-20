9c04208f234540b9964ab00a48b9986807590bcc
lionellei <lionel_lei@yahoo.com>
2014-11-19 22:58:25 -0800

    setup tables is not clickable, need to implement its update action

--- client/templates/testgroup/setup/setup.js
+++ client/templates/testgroup/setup/setup.js
@@ -1,3 +1,20 @@
+///////////////////// Events /////////////////////////////
+Template.setup.events({
+    // Click to edit
+    "click .editable": function(event, template){
+        // Each cell when clicked will create a session variable with key of the cell identifier with value true
+        // Why not just use a Session.get('currentEditingCell') to store the cell identifier?
+        // Because you want to set the particular key to false when the cell is focus out (when user clicks away)
+        // instead of setting Session.set('currentEditingCell', null). Because focus out will also get triggered when
+        // the user click another cell, and that cell's .rendered method will try to set Session['currentEditingCell']
+        // too, so there is chance for conflict.
+        var previousEditingCellIdentifier = Session.get("currentEditingCell");
+        Session.set(previousEditingCellIdentifier, false); // Disable editing for the previous cell.
+        Session.set(event.currentTarget.id, true);
+        Session.set("currentEditingCell", event.currentTarget.id);
+    }
+});
+
 ////////////////// Helpers ////////////////////////
 Template.setup.helpers({
    // Create the attributes for each cell setup row:
