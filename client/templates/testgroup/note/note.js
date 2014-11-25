20ccb8af9730b789324885c26ed0845f86b2b8b7
Lionel Li <lionel_lei@yahoo.com>
2014-11-25 19:22:35 +0000

    use editable_cell for test notes

---
+++ client/templates/testgroup/note/note.js
@@ -0,0 +1,23 @@
+Template.note.helpers({
+    "cellAttributes": function(cell_name) {
+        return {
+           class: "editable list-group-item",
+           id: cell_name + '+' + this._id
+        };
+    }    
+});
+
+Template.note.events({
+    "click .editable": function(event, template){
+        // Each cell when clicked will create a session variable with key of the cell identifier with value true
+        // Why not just use a Session.get('currentEditingCell') to store the cell identifier?
+        // Because you want to set the particular key to false when the cell is focus out (when user clicks away)
+        // instead of setting Session.set('currentEditingCell', null). Because focus out will also get triggered when
+        // the user click another cell, and that cell's .rendered method will try to set Session['currentEditingCell']
+        // too, so there is chance for conflict.
+        var previousEditingCellIdentifier = Session.get("currentEditingCell");
+        Session.set(previousEditingCellIdentifier, false); // Disable editing for the previous cell.
+        Session.set(event.currentTarget.id, true);
+        Session.set("currentEditingCell", event.currentTarget.id);
+    },    
+});
