164b925be42893b97daf27b7e5794e27670a13bd
lionellei <lionel_lei@yahoo.com>
2014-12-02 12:26:09 -0800

    implemented submit action in header configs modal

--- client/templates/helpers/test_header_defaults.js
+++ client/templates/helpers/test_header_defaults.js
@@ -9,8 +9,8 @@ testHeaderDefaults = {
         {name: "compliance_value", label:"Value", allowed_value:"", show:true},
         {name: "compliance_unit", label:"Unit", allowed_value:"", show:true},
         {name: "measure_type", label:"Measure", allowed_value:"V,I", show:true},
-        {name: "measure_min", label:"MIN", allowed_value:"", show:true},
         {name: "calculate", label:"Calculate", allowed_value:"", show:false}, // For calculation based on other measurements.
+        {name: "measure_min", label:"MIN", allowed_value:"", show:true},
         {name: "measure_typ", label:"TYP", allowed_value:"", show:true},
         {name: "measure_max", label:"MAX", allowed_value:"", show:true},
         {name: "measure_unit", label:"Unit", allowed_value:"", show:true}
--- client/templates/testgroup/testgroup.js
+++ client/templates/testgroup/testgroup.js
@@ -164,6 +164,13 @@ Template.addNoteModal.events({
 Template.headerConfigModal.events({
     "click .test-header-add-btn": function () {
 
+    },
+
+    "click .header-config-submit-button": function (event, template) {
+        // After submit, update the columns of the test configs to match the Session variable. 
+        var testgroup = findCurrentTestgroup(this);
+        var headerConfigs = TestHeaderConfigs.findOne({testgroup_id: testgroup._id});
+        TestHeaderConfigs.update(headerConfigs._id, {$set: {columns:Session.get('headerColumns')}});
     }
 });
 
